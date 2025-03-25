4ee3600acf427494794b133b9e005a1850dee764
localhost08888 <nileshjadhav93078@gmail.com>
2025-03-25 16:54:45 +0530

    Update RR.c

--- cpu/codes/RR.c
+++ cpu/codes/RR.c
@@ -1,117 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-typedef struct
-{
-    char pname[20];
-    int at, bt, ct, tat, wt, bt1;
-} Process;
-
-void accept_info(Process p[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter process name: ");
-        scanf("%s", p[i].pname);
-        printf("Enter arrival time: ");
-        scanf("%d", &p[i].at);
-        printf("Enter burst time: ");
-        scanf("%d", &p[i].bt);
-        p[i].bt1 = p[i].bt;
-    }
-}
-
-void simulate_rr(Process p[], int n, int quantum)
-{
-    int time = 0, completed = 0;
-    int queue[n], front = 0, rear = 0;
-    int in_queue[n];
-    for (int i = 0; i < n; i++)
-        in_queue[i] = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (p[i].at == 0)
-        {
-            queue[rear++] = i;
-            in_queue[i] = 1;
-        }
-    }
-
-    while (completed < n)
-    {
-        if (front == rear)
-        {
-            time++;
-            for (int i = 0; i < n; i++)
-            {
-                if (p[i].at == time && !in_queue[i])
-                {
-                    queue[rear++] = i;
-                    in_queue[i] = 1;
-                }
-            }
-            continue;
-        }
-
-        int idx = queue[front++];
-        if (p[idx].bt1 > quantum)
-        {
-            time += quantum;
-            p[idx].bt1 -= quantum;
-        }
-        else
-        {
-            time += p[idx].bt1;
-            p[idx].bt1 = 0;
-            p[idx].ct = time;
-            p[idx].tat = p[idx].ct - p[idx].at;
-            p[idx].wt = p[idx].tat - p[idx].bt;
-            completed++;
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            if (p[i].at <= time && p[i].bt1 > 0 && !in_queue[i])
-            {
-                queue[rear++] = i;
-                in_queue[i] = 1;
-            }
-        }
-
-        if (p[idx].bt1 > 0)
-        {
-            queue[rear++] = idx;
-        }
-    }
-}
-
-void print_output(Process p[], int n)
-{
-    float avg_tat = 0, avg_wt = 0;
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%s\t%d\t%d\t%d\t%d\t%d\n", p[i].pname, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
-        avg_tat += p[i].tat;
-        avg_wt += p[i].wt;
-    }
-    printf("\nAverage Turnaround Time = %.2f\n", avg_tat / n);
-    printf("Average Waiting Time = %.2f\n", avg_wt / n);
-}
-
-int main()
-{
-    srand(time(0));
-    int n, quantum;
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-    printf("Enter time quantum: ");
-    scanf("%d", &quantum);
-    Process p[n];
-    accept_info(p, n);
-    simulate_rr(p, n, quantum);
-    print_output(p, n);
-    return 0;
-}
+  #include <stdio.h> 
+#define MAX 30 
+ 
+int main()  
+{ 
+    int n, i, j, quantum; 
+    int p[MAX], at[MAX], bt[MAX], ct[MAX], tat[MAX], wt[MAX], remaining_bt[MAX]; 
+    int time = 0, completed = 0; 
+    float awt = 0, atat = 0; 
+ 
+    // Input: Number of Processes 
+    printf("Enter the number of processes: "); 
+    scanf("%d", &n); 
+ 
+    // Taking input for process numbers 
+    printf("Enter the process numbers: "); 
+    for (i = 0; i < n; i++) 
+     { 
+        scanf("%d", &p[i]); 
+    } 
+ 
+    // Taking input for arrival times of processes 
+    printf("Enter the arrival time for each process: "); 
+    for (i = 0; i < n; i++)  
+    { 
+        scanf("%d", &at[i]); 
+    } 
+ 
+    // Taking input for burst times of processes 
+    printf("Enter the burst time for each process: "); 
+    for (i = 0; i < n; i++) 
+     { 
+        scanf("%d", &bt[i]); 
+        remaining_bt[i] = bt[i]; // Initialize remaining burst time 
+    } 
+ 
+    // Taking time quantum input 
+    printf("Enter the time quantum: "); 
+    scanf("%d", &quantum); 
+  
+    // Round Robin Scheduling 
+    int queue[MAX], front = 0, rear = 0; 
+    int visited[MAX] = {0}; // To track if a process is already in queue 
+ 
+    queue[rear++] = 0; // Start with the first process 
+    visited[0] = 1; 
+ 
+    while (completed != n) 
+     { 
+        int index = queue[front++]; 
+         
+        // Execute the process for `quantum` or remaining burst time 
+        int exec_time = (remaining_bt[index] > quantum) ? quantum : remaining_bt[index]; 
+        time += exec_time; 
+        remaining_bt[index] -= exec_time; 
+ 
+        // Check if new processes have arrived and add them to the queue 
+        for (i = 0; i < n; i++)  
+        { 
+            if (at[i] <= time && !visited[i] && remaining_bt[i] > 0) 
+             { 
+                queue[rear++] = i; 
+                visited[i] = 1; 
+            } 
+        } 
+ 
+        // If process is not completed, put it back in queue 
+        if (remaining_bt[index] > 0)  
+        { 
+            queue[rear++] = index; 
+        }  
+        else  
+        { // If process is completed, store completion time 
+            ct[index] = time; 
+            completed++; 
+        } 
+    } 
+ 
+    // Calculate Turnaround Time (TAT) and Waiting Time (WT) 
+    for (i = 0; i < n; i++)  
+    { 
+        tat[i] = ct[i] - at[i]; // TAT = CT - AT 
+        atat += tat[i]; 
+ 
+        wt[i] = tat[i] - bt[i]; // WT = TAT - BT 
+        awt += wt[i]; 
+    } 
+ 
+    // Calculate average waiting time and turnaround time 
+    awt /= n; 
+    atat /= n; 
+ 
+    // Display results 
+    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n"); 
+    for (i = 0; i < n; i++) 
+     {  
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i]); 
+    } 
+ 
+    // Printing average values 
+    printf("\nAverage Waiting Time: %.2f", awt); 
+    printf("\nAverage Turnaround Time: %.2f\n", atat); 
+ 
+    return 0; 
+} 
