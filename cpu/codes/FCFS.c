e633bbd2541ae31eb8fe030934670874f5e8620c
localhost08888 <nileshjadhav93078@gmail.com>
2025-03-25 16:00:58 +0530

    Update FCFS.c

--- cpu/codes/FCFS.c
+++ cpu/codes/FCFS.c
@@ -1,119 +1,124 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+ #include <stdio.h>
 
-typedef struct
+#define MAX 30
+
+int main() 
 {
-    char pname[20];
-    int at;
-    int bt;
-    int ct;
-    int tat;
-    int wt;
-    int bt1;
-} Process;
+    int n, i, j;
+    int p[MAX], at[MAX], bt[MAX], ct[MAX], wt[MAX], tat[MAX];
+    float awt = 0, atat = 0;
 
-void accept_info(Process p[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter process name: ");
-        scanf("%s", p[i].pname);
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
 
-        printf("Enter arrival time: ");
-        scanf("%d", &p[i].at);
-
-        printf("Enter first CPU burst time: ");
-        scanf("%d", &p[i].bt);
-
-        p[i].bt1 = p[i].bt;
+    printf("Enter the process numbers: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &p[i]);
     }
-}
 
-void sort_by_arrival(Process p[], int n)
-{
+    
+    printf("Enter the arrival time for each process: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &at[i]);
+    }
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (p[i].at > p[j].at)
-            {
-                Process temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
+    
+    printf("Enter the burst time for each process: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &bt[i]);
+    }
+
+    
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (at[j] > at[j + 1]) {
+              
+                int temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
+
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
     }
-}
 
-void simulate_fcfs(Process p[], int n)
-{
     int time = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (time < p[i].at)
-        {
-            time = p[i].at;
+    for (i = 0; i < n; i++) {
+        if (time < at[i]) {
+            time = at[i]; 
         }
-        time += p[i].bt1;
-        p[i].ct = time;
-        p[i].tat = p[i].ct - p[i].at;
-        p[i].wt = p[i].tat - p[i].bt;
-
-        time += 2;
-
-        p[i].bt1 = rand() % 10 + 1;
+        time += bt[i]; 
+        ct[i] = time;  
     }
-}
 
-void print_output(Process p[], int n)
-{
-    float avg_tat = 0, avg_wt = 0;
+   
+    for (i = 0; i < n; i++) {
+        tat[i] = ct[i] - at[i];
+        atat += tat[i];
+
+        wt[i] = tat[i] - bt[i]; 
+        awt += wt[i];
+    }
+
+   
+    awt /= n;
+    atat /= n;
+
+    
     printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%s\t%d\t%d\t%d\t%d\t%d\n",
-               p[i].pname, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
-        avg_tat += p[i].tat;
-        avg_wt += p[i].wt;
+    for (i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
     }
-    printf("\nAverage Turnaround Time = %.2f\n", avg_tat / n);
-    printf("Average Waiting Time = %.2f\n", avg_wt / n);
-}
 
-void print_gantt_chart(Process p[], int n)
-{
+    
+    printf("\nAverage Waiting Time: %.2f", awt);
+    printf("\nAverage Turnaround Time: %.2f\n", atat);
+
+   
     printf("\nGantt Chart:\n");
-    int time = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (time < p[i].at)
-        {
-            printf("| Idle (%d-%d) ", time, p[i].at);
-            time = p[i].at;
-        }
-        printf("| %s (%d-%d) ", p[i].pname, time, time + p[i].bt);
-        time += p[i].bt;
-        printf("| I/O (%d-%d) ", time, time + 2);
-        time += 2;
+
+    
+    printf(" ");
+    for (i = 0; i < n; i++) {
+        printf("-------");
     }
-    printf("|\n");
-}
+    printf("\n|");
 
-int main()
-{
-    srand(time(0));
-    int n;
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
+    
+    time = 0;
+    for (i = 0; i < n; i++) {
+        if (time < at[i]) {
+            printf(" Idle |");
+            time = at[i];
+        }
+        printf(" P%d |", p[i]);
+        time += bt[i];
+    }
 
-    Process p[n];
-    accept_info(p, n);
-    sort_by_arrival(p, n);
-    simulate_fcfs(p, n);
-    print_output(p, n);
-    print_gantt_chart(p, n);
+    
+    printf("\n ");
+    for (i = 0; i < n; i++) {
+        printf("-------");
+    }
+
+   
+    printf("\n0");
+    time = 0;
+    for (i = 0; i < n; i++) {
+        if (time < at[i]) {
+            printf("\t%d", at[i]);
+            time = at[i];
+        }
+        time += bt[i];
+        printf("\t%d", time);
+    }
+    printf("\n");
 
     return 0;
 }
